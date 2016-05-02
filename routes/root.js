2f446fe36597f10adf3eee95670b4e770a82d787
ixxra <garciamx@gmail.com>
2016-05-02 02:15:31 -0500

    keywords in view

--- routes/root.js
+++ routes/root.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var Book = require('../models/book');
 
 router.get('/', function(req, res, next) {
-  var latestBooks = Book.find(null, null, {sort: {mtime: 1}, limit: 10}, function(err, docs){
+  var latestBooks = Book.find(null, null, {sort: {mtime: -1}, limit: 10}, function(err, docs){
     if (err){
       next(err);
     } else {
